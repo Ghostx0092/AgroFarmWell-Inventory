9e1060f55c3cc0f28a749850f356570f3b3582b6
Ghostx0092 <rohansharma4896@gmail.com>
2024-04-03 22:51:20 +0530

    inventory

---
+++ inventoryClient/src/App.js
@@ -0,0 +1,165 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
+import { useSearchParams } from "react-router-dom";
+import './App.css';
+
+function App() {
+  // const [email, setEmail] = useState('');
+  const [cropName, setCropName] = useState('');
+  const [amount, setAmount] = useState(0);
+  const [newCropName, setNewCropName] = useState('');
+  const [cart, setCart] = useState([]);
+  const [message, setMessage] = useState('');
+  
+  
+  const url = window.location.href;
+  const email = url.substring(url.lastIndexOf('/') + 1);
+
+
+  const handleAddToCart = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post('https://inventoryserver.onrender.com/addtocart', {
+        email,
+        cropName,
+        amount
+      });
+      setMessage(res.data.message);
+    } catch (error) {
+      setMessage(error.response.data.message);
+    }
+  };
+
+  const handleDeleteCrop = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.delete(`https://inventoryserver.onrender.com/deletecrop/${email}/${cropName}`);
+      setMessage(res.data.message);
+    } catch (error) {
+      setMessage(error.response.data.message);
+    }
+  };
+
+  const handleGetCart = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.get(`https://inventoryserver.onrender.com/cart/${email}`);
+      setCart(res.data.cart);
+    } catch (error) {
+      setMessage(error.response.data.message);
+    }
+  };
+
+  const handleUpdateCrop = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.put(`https://inventoryserver.onrender.com/updatecrop/${email}/${cropName}`, {
+        newCropName,
+        amount
+      });
+      setMessage(res.data.message);
+    } catch (error) {
+      setMessage(error.response.data.message);
+    }
+  };
+  
+  return (
+    
+    <div class="mainBody">
+      <h1 class="mainTitle">Inventory</h1>
+      {/* <p>The current URL is: {email}</p> */}
+      <div class="upper">
+        <div class="section_1 section">
+          <h1>Add to cart</h1>
+          <form onSubmit={handleAddToCart}>
+            {/* <label>
+              <input type="email" placeholder="Email" required value={email}  />
+            </label> */}
+            <label>
+              <input type="text" placeholder="Crop Name" required value={cropName} onChange={(e) => setCropName(e.target.value)} />
+            </label>
+            <label>
+              <input type="number" placeholder="Amount" required value={amount} onChange={(e) => setAmount(Number(e.target.value))} />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
+        <div class="section_2 section">
+          <h1>Delete crop</h1>
+          <form onSubmit={handleDeleteCrop}>
+            {/* <label>
+              <input type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label> */}
+            <label>
+              <input type="text" placeholder="Crop name" required value={cropName} onChange={(e) => setCropName(e.target.value)} />
+            </label>
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+      </div>
+
+      <div class="lower">
+        <div class="section_3 section">
+          <h1>View cart</h1>
+          <form onSubmit={handleGetCart}>
+            {/* <label>
+              <input type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label> */}
+            <button type="submit">Submit</button>
+          </form>
+        </div>
+
+        <div class="section_4 section">
+          <h1>Update crop</h1>
+          <form onSubmit={handleUpdateCrop}>
+            {/* <label>
+              <input type="email" placeholder="Email" required value={email} onChange={(e) => setEmail(e.target.value)} />
+            </label> */}
+            <label>
+              <input type="text" placeholder="Crop name" required value={cropName} onChange={(e) => setCropName(e.target.value)} />
+            </label>
+            <label>
+              <input type="text" required placeholder="new crop name" value={newCropName} onChange={(e) => setNewCropName(e.target.value)} />
+            </label>
+            <label>
+              <input type="number" placeholder="Amount" required value={amount} onChange={(e) => setAmount(Number(e.target.value))} />
+            </label>
+            <button type="submit" onclick="scrollToDiv()">Submit</button>
+          </form>
+        </div>
+      </div>
+      <div class="table">
+        <h2>Items:</h2>
+        {cart.length > 0 && (
+          <table>
+            <thead>
+              <tr>
+                <th>Crop Name</th>
+                <th>Amount (kg)</th>
+              </tr>
+            </thead>
+            <tbody>
+              {cart.map((item, index) => (
+                <tr key={index}>
+                  <td>{item.cropName}</td>
+                  <td>{item.amount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        )}
+      </div>
+
+
+
+      <p class="alert">{message}</p>
+    </div>
+  );
+}
+
+
+export default App;
